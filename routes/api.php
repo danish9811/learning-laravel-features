2b86fab563decd0c06f22861d66c762cbb1320c7
danish mehmood <danish.mehmood@otif-solutions.com>
2022-09-09 11:32:45 +0500

    How routes can be made better

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,13 @@ Route::get('/get-weather-via-http', [MesonetApiController::class, 'getMesonetApi
 // issue in this endpoint | laravel version with the otif lib
 Route::get('/get-weather-via-otif-curl', [MesonetApiController::class, 'getMesonetApiResultViaOtifCurl']);
 Route::get('/get-weather-via-curl', [MesonetApiController::class, 'getMesonetApiResultViaCurl']);
+
+
+// by updating the otif-curl will fix this
+// all the api routes are shorten down | how nice these are
+Route::controller(MesonetApiController::class)->group(static function() {
+    Route::get('/get-weather-via-curl', 'getMesonetApiResultViaOtifCurl');
+    Route::get('/get-weather-via-http', 'getMesonetApiResultViaHttp');
+    Route::get('/get-weather-via-curl', 'getMesonetApiResultViaCurl');
+});
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ Route::post('/register-submit', [PassportAuthController::class, 'passportAuthReg
 Route::get('/chart', [PassportAuthController::class, 'showApexChart'])
     ->middleware('auth')
     ->name('apex-chart');
+
+
+// all the above routes can be shorten by this method
+Route::controller(PassportAuthController::class)->group(static function() {
+    Route::get('/login', 'showLoginForm')->name('login');   // the same as above
+    Route::get('/register', 'showRegisterForm')->name('register');  // the same as above
+    Route::post('/login-submit', 'passportAuthLoginSubmit');
+    Route::post('/register-submit', 'passportAuthRegisterSubmit');
+    Route::get('/chart', 'showApexChart')->middleware('auth')->name('apex-chart');
+});
+
+
+
+
+// all of these routes can be converted to laravel 9 routes, if group of routes comes under single controller, you can combile them
