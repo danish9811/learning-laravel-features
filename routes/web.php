4d679bfcf5324dfe494c71d6aa054586d8c5ae2f
danish mehmood <danish.mehmood@otif-solutions.com>
2022-10-19 17:36:04 +0500

    way of defining routes updated

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,9 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\PassportAuthController;
 use Illuminate\Support\Facades\Route;
 
-
-// these routes can be made simpler
-//
-//Route::view('/', 'dashboard');
-//Route::resource('/books', BookController::class);
-//
-//Route::get('/login', [PassportAuthController::class, 'showLoginForm'])->name('login');
-//Route::get('/register', [PassportAuthController::class, 'showRegisterForm'])->name('register');
-//
-//Route::post('/login-submit', [PassportAuthController::class, 'passportAuthLoginSubmit']);
-//Route::post('/register-submit', [PassportAuthController::class, 'passportAuthRegisterSubmit']);
-//
-//Route::get('/chart', [PassportAuthController::class, 'showApexChart'])
-//    ->middleware('auth')
-//    ->name('apex-chart');
-
-
-
-// all the above routes can be shorten by this method, phpstorm does not understand these routes
-Route::controller(PassportAuthController::class)->group(static function() {
+Route::controller(PassportAuthController::class)->group(static function () {
     Route::get('/login', 'showLoginForm')->name('login');   // the same as above
     Route::get('/register', 'showRegisterForm')->name('register');  // the same as above
     Route::post('/login-submit', 'passportAuthLoginSubmit');
