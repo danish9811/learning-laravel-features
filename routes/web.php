b52c0cfecafd81fb4579efa33c1dfecf4502f82a
danish mehmood <danish.mehmood@otif-solutions.com>
2022-10-19 17:46:05 +0500

    search feature practiced with ajax

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PassportAuthController::class)->group(static function () {
@@ -9,4 +10,9 @@ Route::controller(PassportAuthController::class)->group(static function () {
     Route::post('/login-submit', 'passportAuthLoginSubmit');
     Route::post('/register-submit', 'passportAuthRegisterSubmit');
     Route::get('/chart', 'showApexChart')->middleware('auth')->name('apex-chart');
+});
+
+Route::controller(SearchController::class)->group(static function () {
+    Route::get('/search', 'index')->name('search');
+    Route::get('/autocomplete', 'autocomplete')->name('autocomplete');
 });
