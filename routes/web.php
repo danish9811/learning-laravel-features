7876215a09a4497cbf6940ce801e7102f8bd87b0
danish mehmood <danish.mehmood@otif-solutions.com>
2022-10-25 12:50:48 +0500

    comments removed

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PassportAuthController::class)->group(static function () {
-    Route::get('/login', 'showLoginForm')->name('login');   // the same as above
-    Route::get('/register', 'showRegisterForm')->name('register');  // the same as above
+    Route::get('/login', 'showLoginForm')->name('login');
+    Route::get('/register', 'showRegisterForm')->name('register');
     Route::post('/login-submit', 'passportAuthLoginSubmit');
     Route::post('/register-submit', 'passportAuthRegisterSubmit');
     Route::get('/chart', 'showApexChart')->middleware('auth')->name('apex-chart');
