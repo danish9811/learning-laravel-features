f138b04e9f39400bccc3a211706fea6e3dda7f62
danish mehmood <danish.mehmood@otif-solutions.com>
2022-11-28 23:45:40 +0500

    Api routes cleaned

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/get-weather-via-http', [MesonetApiController::class, 'getMesonetApiResultViaHttp']);
-
-// issue in this endpoint | laravel version with the otif lib
 Route::get('/get-weather-via-otif-curl', [MesonetApiController::class, 'getMesonetApiResultViaOtifCurl']);
 Route::get('/get-weather-via-curl', [MesonetApiController::class, 'getMesonetApiResultViaCurl']);
-
-
-// by updating the otif-curl will fix this
-// all the api routes are shorten down | how nice these are
-Route::controller(MesonetApiController::class)->group(static function() {
-    Route::get('/get-weather-via-curl', 'getMesonetApiResultViaOtifCurl');
-    Route::get('/get-weather-via-http', 'getMesonetApiResultViaHttp');
-    Route::get('/get-weather-via-curl', 'getMesonetApiResultViaCurl');
-});
 
